770605a75ffd00e68c5e9b79663d71cf1b0b3087
Oliver Andrich <oliver@andrich.me>
2025-02-04 10:14:52 +0100

    Stick to pytest, Oliver

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -16,13 +16,6 @@ uv sync --quiet --all-extras
 echo "\033[32m✓\033[0m Created default tailwind.config.js."
 uv run python manage.py tailwind build 2>&1 >/dev/null
 
-{% if cookiecutter.use_pycharm %}
-echo "\033[32m✓\033[0m Configured PyCharm integration for Tailwind CSS and htmx."
-uv run python manage.py tailwind install_pycharm_workaround 2>&1 >/dev/null
-curl -s -L -O https://raw.githubusercontent.com/bigskysoftware/htmx/refs/heads/master/editors/jetbrains/htmx.web-types.json
-echo '{ "devDependencies": {"tailwindcss": "latest"}, "web-types": "./htmx.web-types.json" }' >package.json
-{% endif %}
-
 {% if cookiecutter.license == "EUPL-1.2" %}
 echo "\033[32m✓\033[0m Created EUPL-1.2 license file."
 curl -s -L https://interoperable-europe.ec.europa.eu/sites/default/files/custom-page/attachment/2020-03/EUPL-1.2%20EN.txt >LICENSE.txt
@@ -40,7 +33,7 @@ git add -A . 2>&1 >/dev/null
 git commit -q -m "Initial commit"
 
 echo "\033[32m✓\033[0m Installed pre-commit hook."
-uv tool run pre-commit install
+uvx --with pre-commit-uv pre-commit install
 
 echo
 echo "\033[32mHappy coding!\033[0m"
--- {{ cookiecutter.project_folder }}/tests/test_truth.py
+++ {{ cookiecutter.project_folder }}/tests/test_truth.py
@@ -1,6 +1,2 @@
-from django.test import TestCase
-
-
-class TestTruthTestCase(TestCase):
-    def test_truth(self):
-        assert True
+def test_truth():
+    assert True
